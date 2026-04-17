3bbaba8b001509f8348bb8d5cc5dccd000b6efa5
Naibitx <naibys.alzugarayofficial@gmail.com>
2026-04-17 15:34:28 -0400

    worked on main and config file

---
+++ backend/app/config.py
@@ -0,0 +1,24 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):#this wll load enviornment variables from our .env
+
+    #app settings below
+    app_name: str = "AI Comparator"
+    debug: bool= True
+
+    #database setting bellow
+    database_url:str = ""
+
+    #api keys settings bellow
+
+    #paths bellow
+    workspace_dir: str= "./workspace"
+
+    class Config:
+        env_file=".env" #loads our env file
+
+#to load just once
+@lru_cache
+def get_settings():
+    return Settings
---
+++ backend/app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CROSMiddleware #this to connect frontedn and backend
+#below we have our router imports 
+from app.routes.tasks import router as t_Router
+from app.routes.runs import router as ru_Router
+from app.routes.reports import router as re_Router
+from app.routes.settings import router as s_Router
+
+app= FastAPI(title= "AI Comparator Assistant")
+app.add_middleware(
+    CROSMiddleware,
+    allow_origins= ["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers= ["*"]
+)
+
+@app.get("/")
+def root():
+    return{"message": "Tesitng backend, backend is running YAYY"}
+
+#below we are registering the routes
+app.include_router(t_Router, prefix="/tasks", tags=["Tasks"])
+app.include_router(re_Router, prefix="/reports", tags=["Reports"])
+app.include_router(ru_Router, prefix="/runs", tags=["Runs"])
+app.include_router(s_Router, prefix="/settings", tags=["Settings"])
